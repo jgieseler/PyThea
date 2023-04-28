fe291ab730d0a2ef67d10d4bd9d8e534cf0f795f
Jan Gieseler <39489154+jgieseler@users.noreply.github.com>
2023-04-28 16:34:16 +0300

    Update __init__.py

--- PyThea/__init__.py
+++ PyThea/__init__.py
@@ -2,9 +2,12 @@ from .version import version as __version__
 import streamlit.web.bootstrap
 from astropy.utils.data import get_pkg_data_filename
 
-# get full path to PyThea_app.py
-fullpath = get_pkg_data_filename('PyThea_app.py', package='PyThea')
+def PyThea():
+  # get full path to PyThea_app.py
+  fullpath = get_pkg_data_filename('PyThea_app.py', package='PyThea')
 
-# execute streamlit app
-# run ?streamlit.web.bootstrap.run to get info on the options
-streamlit.web.bootstrap.run(fullpath, '', [], [])
+  # execute streamlit app
+  # run ?streamlit.web.bootstrap.run to get info on the options
+  streamlit.web.bootstrap.run(fullpath, '', [], [])
+  
+  return
